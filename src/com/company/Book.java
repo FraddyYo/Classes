f5c982834a435b50d1e12207aee89eb5160d22cb
Georgii <loveOlechka@mail.ru>
2022-01-30 17:18:41 +0300

    сделал)

---
+++ src/com/company/Book.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Book {
+    private String name;
+    private int yearOfPublication;
+
+    public Book(String name, int yearOfPublication) {
+        this.name = name;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getName() {
+        return this.name;
+
+    }
+    public int getYearOfPublication(){
+        return this.yearOfPublication;
+    }
+    public void setYearOfPublication(int yearOfPublication){
+        this.yearOfPublication = yearOfPublication;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,20 @@ package com.company;
 
 public class Main {
 
+
+
+
     public static void main(String[] args) {
-	// write your code here
+
+        Book nameBook = new Book("Мертвые души", 1842);
+        System.out.println("nameBook.name = " + nameBook.getName());
+        System.out.println("nameBook.yearOfPublication = " + nameBook.getYearOfPublication());
+        Author nameAuthor = new Author("Николай Васильевич Гоголь");
+        Author newAuthor2 = new Author("Лев Николаевич Толстой");
+        System.out.println("nameAuthor.name = " + nameAuthor.getName());
+        nameBook.setYearOfPublication(1852);
+        System.out.println("nameBook.getYearOfPublication() = " + nameBook.getYearOfPublication());
+        Book newBook2 = new Book("Война и мир", 1867 );
+
     }
 }
